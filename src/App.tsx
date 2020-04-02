d1ab30cefefe84d8649502af9d24910558a64901
hjhun-Macmini <hjhun0504@gmail.com>
2020-04-03 01:38:59 +0900

    props로 내려주는 함수들을 useCallback으로 감싸 최적화

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import TodoAdd from '@components/TodoAdd';
 import TodoList from '@components/TodoList';
 
@@ -8,25 +8,38 @@ const App = (): JSX.Element => {
   const [todos, setTodos] = useState(todoDummy);
   const nextId = useRef(4);
 
-  const handleAddTodo = (text: string, targetTime: number): void => {
-    const updatedTodos = todos.concat({ id: nextId.current, text, targetTime });
-    setTodos(updatedTodos);
-    nextId.current++;
-  };
+  const handleAddTodo = useCallback(
+    (text: string, targetTime: number): void => {
+      const updatedTodos = todos.concat({
+        id: nextId.current,
+        text,
+        targetTime,
+      });
+      setTodos(updatedTodos);
+      nextId.current++;
+    },
+    [todos],
+  );
 
-  const handleEditTodoText = (id: number, editedText: string): void => {
-    const updatedTodos = todos.map((todo) =>
-      todo.id === id ? { ...todo, text: editedText } : todo,
-    );
-    setTodos(updatedTodos);
-  };
+  const handleEditTodoText = useCallback(
+    (id: number, editedText: string): void => {
+      const updatedTodos = todos.map((todo) =>
+        todo.id === id ? { ...todo, text: editedText } : todo,
+      );
+      setTodos(updatedTodos);
+    },
+    [todos],
+  );
 
-  const handleEditTodoTime = (id: number, editedTime: number): void => {
-    const updatedTodos = todos.map((todo) =>
-      todo.id === id ? { ...todo, targetTime: editedTime } : todo,
-    );
-    setTodos(updatedTodos);
-  };
+  const handleEditTodoTime = useCallback(
+    (id: number, editedTime: number): void => {
+      const updatedTodos = todos.map((todo) =>
+        todo.id === id ? { ...todo, targetTime: editedTime } : todo,
+      );
+      setTodos(updatedTodos);
+    },
+    [todos],
+  );
 
   return (
     <div>
