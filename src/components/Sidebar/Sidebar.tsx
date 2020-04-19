d616ac1bc6a40ddb6c99ee4a8de8a5b4e2bbeb8d
hjhun-Macmini <hjhun0504@gmail.com>
2020-04-19 18:05:29 +0900

    Feature: 사이드바 메뉴 정적인 컴포넌트 추가

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import SidebarItem from '@components/Sidebar/SidebarItem/SidebarItem';
+
 import './Sidebar.scss';
 
 const Sidebar = (): JSX.Element => {
-  return <aside className="Sidebar">Sidebar</aside>;
+  return (
+    <aside className="Sidebar">
+      <SidebarItem icon="today" text="오늘" />
+      <SidebarItem icon="history" text="기록" />
+    </aside>
+  );
 };
 
 export default Sidebar;
---
+++ src/components/Sidebar/SidebarItem/SidebarItem.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './SidebarItem.scss';
+
+import { FaRegCalendar, FaHistory } from 'react-icons/fa';
+
+interface Props {
+  icon: 'today' | 'history';
+  text: string;
+}
+
+const SidebarItem = (props: Props): JSX.Element => {
+  const { icon, text } = props;
+  let svgIcon;
+  switch (icon) {
+    case 'today':
+      svgIcon = <FaRegCalendar />;
+      break;
+    case 'history':
+      svgIcon = <FaHistory />;
+      break;
+  }
+
+  return (
+    <div>
+      {svgIcon}
+      <span>{text}</span>
+    </div>
+  );
+};
+
+export default SidebarItem;
