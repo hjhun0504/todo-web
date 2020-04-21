3971b3646b8761c38ee28e0a757724ff4a5a7c62
hjhun-Macmini <hjhun0504@gmail.com>
2020-04-21 18:44:06 +0900

    Styling: 상단바 메뉴 아이콘 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,10 +3,14 @@ import React from 'react';
 import './Header.scss';
 
 import { FaSearch } from 'react-icons/fa';
+import { MdMenu } from 'react-icons/md';
 
 const Header = (): JSX.Element => {
   return (
     <header className="Header">
+      <div className="menu">
+        <MdMenu />
+      </div>
       <div>
         <FaSearch className="icon" />
         <input className="search edit-box" placeholder="검색" />
