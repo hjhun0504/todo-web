2271b2eb6f1319f09ecac683095c63edd02effc9
hjhun-Macmini <hjhun0504@gmail.com>
2020-04-26 14:36:14 +0900

    Styling: 타이틀 컴포넌트 스타일링

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -10,39 +10,44 @@ interface Props {
 
 const week = ['일', '월', '화', '수', '목', '금', '토'];
 
-const getTitle = (menu: SidebarItems): JSX.Element => {
-  switch (menu) {
+const Title = (props: Props): JSX.Element => {
+  const { currentItem } = props;
+  const moreButton = React.createRef<HTMLDivElement>();
+
+  const handleClick = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  ): void => {
+    event.stopPropagation();
+  };
+
+  switch (currentItem) {
     case 'today':
       const time = new Date();
       const month = time.getMonth() + 1;
       const date = time.getDate();
       const day = week[time.getDay()];
       return (
-        <>
+        <div className="Title">
           <div className="title">
             <span className="text">오늘</span>
-            <IoIosMore className="today-icon icon" />
+            <div className="icon" ref={moreButton} onClick={handleClick}>
+              <IoIosMore />
+            </div>
           </div>
           <div className="date">
             {month}월 {date}일, {day}요일
           </div>
-        </>
+        </div>
       );
     case 'history':
       return (
-        <>
+        <div className="Title">
           <div className="title">
             <span className="text">기록</span>
           </div>
-        </>
+        </div>
       );
   }
-};
-
-const Title = (props: Props): JSX.Element => {
-  const { currentItem } = props;
-
-  return <div className="Title">{getTitle(currentItem)}</div>;
 };
 
 export default Title;
