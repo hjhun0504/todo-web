4b83ee68963433a5037d4d4765bbde0052d7246b
hjhun-Macmini <hjhun0504@gmail.com>
2020-05-01 16:54:36 +0900

    Refactor: 타임라인 코드 리팩토링

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -11,24 +11,13 @@ const getHours = (time: Date): number => {
   return time.getHours() + time.getMinutes() / 60;
 };
 
-const getPosition = (
-  startTime: Date,
-  timelineStartTime: number,
-  timelineEndTime: number,
-): number => {
-  const hours = getHours(startTime) - timelineStartTime;
-  return (hours / (timelineEndTime - timelineStartTime)) * 100;
-};
-
-const getWidth = (
-  startTime: Date,
-  finishTime: Date,
-  timelineStartTime: number,
-  timelineEndTime: number,
-): number => {
-  const consumedTime = getHours(finishTime) - getHours(startTime);
-  const width = (consumedTime / (timelineEndTime - timelineStartTime)) * 100;
-  return width > 1 ? width : 1; // 최소 1%의 폭을 가진다.
+const getAdjustedSize = (originalSize: number): number => {
+  const minimumSize = 1;
+  if (originalSize > minimumSize) {
+    return originalSize;
+  } else {
+    return minimumSize;
+  }
 };
 
 interface Props {
@@ -37,17 +26,7 @@ interface Props {
 
 const Timeline = (props: Props): JSX.Element => {
   const { todos } = props;
-
-  // 완료된 작업들을 시작한 시간순으로 오름차순으로 정렬한다.
-  const sortedTodos = produce(todos, (draft) => {
-    return draft.sort((a, b) => {
-      if (a.startTime && b.startTime) {
-        return b.startTime?.getTime() - a.startTime?.getTime();
-      } else {
-        return 0;
-      }
-    });
-  });
+  const [tasks, setTasks] = useState<(JSX.Element | undefined)[]>([]);
 
   // const currentHour = new Date().getHours() + 2;
   // const timelineStartTime =
@@ -66,75 +45,82 @@ const Timeline = (props: Props): JSX.Element => {
     );
   }
 
-  const [items, setItems] = useState<(JSX.Element | undefined)[]>();
+  // 작업들을 시작 시간(오름차순)으로 정렬
+  const sortedTodos = produce(todos, (draft) => {
+    return draft.sort((a, b) => {
+      if (a.startTime && b.startTime) {
+        return b.startTime?.getTime() - a.startTime?.getTime();
+      } else {
+        return 0;
+      }
+    });
+  });
+
+  const taskElements = (): (JSX.Element | undefined)[] => {
+    return sortedTodos.map((todo, index) => {
+      if (!todo.startTime) return; // 시작 되지 않은 todo는 없음(타입 체킹용)
+
+      const startHours = getHours(todo.startTime) - timelineStartTime;
+      const left = (startHours / (timelineEndTime - timelineStartTime)) * 100;
+
+      let targetTime;
+      let progress = null;
+      if (todo.finishTime) {
+        targetTime = todo.finishTime;
+      } else {
+        targetTime = new Date(todo.startTime);
+        targetTime.setMinutes(targetTime.getMinutes() + todo.targetMinutes);
+
+        const elapsedMinutes =
+          (new Date().getTime() - todo.startTime.getTime()) / 60000;
+        const progressPercent = (elapsedMinutes / todo.targetMinutes) * 100;
+
+        let progressWidth;
+        if (progressPercent < 100) {
+          progressWidth = progressPercent;
+        } else {
+          const progressSize = (elapsedMinutes / (60 * 24)) * 100;
+          const targetRealSize = (todo.targetMinutes / (60 * 24)) * 100;
+          const targetSize = getAdjustedSize(targetRealSize);
+          if (progressSize > targetSize) {
+            progressWidth = (progressSize / targetSize) * 100;
+          } else {
+            progressWidth = 100;
+          }
+        }
+
+        progress = (
+          <div
+            className={cn('progress', { exceed: progressPercent >= 100 })}
+            style={{ width: `${progressWidth}%` }}
+          ></div>
+        );
+      }
+
+      const consumedHours = getHours(targetTime) - getHours(todo.startTime);
+      const taskRealWidth =
+        (consumedHours / (timelineEndTime - timelineStartTime)) * 100;
+      const taskWidth = getAdjustedSize(taskRealWidth);
+
+      return (
+        <div key={index}>
+          <Tippy content={`${todo.text}`}>
+            <div
+              className={cn('item', { done: todo.finishTime })}
+              style={{ left: `${left}%`, width: `${taskWidth}%` }}
+            >
+              <div className="item-text">{todo.text}</div>
+              {progress}
+            </div>
+          </Tippy>
+        </div>
+      );
+    });
+  };
 
   useEffect(() => {
     const loop = setInterval(() => {
-      const result = sortedTodos.map((todo, index) => {
-        if (!todo.startTime) return;
-        let objectTime;
-        let result;
-        let progress = <></>;
-        if (todo.finishTime) {
-          objectTime = todo.finishTime;
-        } else {
-          objectTime = new Date(todo.startTime);
-          objectTime.setMinutes(objectTime.getMinutes() + todo.targetMinutes);
-
-          const elapsedMinutes =
-            (new Date().getTime() - todo.startTime.getTime()) / 60000;
-          const percent = (elapsedMinutes / todo.targetMinutes) * 100;
-
-          if (percent < 100) {
-            result = percent;
-          } else {
-            const realSize = (elapsedMinutes / (60 * 24)) * 100;
-            const targetRealSize = (todo.targetMinutes / (60 * 24)) * 100;
-            const targetSize = targetRealSize > 1 ? targetRealSize : 1;
-            if (realSize > targetSize) {
-              result = (realSize / targetSize) * 100;
-            } else {
-              result = 100;
-            }
-          }
-
-          progress = (
-            <div
-              className={cn('progress', { exceed: percent >= 100 })}
-              style={{ width: `${result}%` }}
-            ></div>
-          );
-        }
-
-        const left = getPosition(
-          todo.startTime,
-          timelineStartTime,
-          timelineEndTime,
-        );
-
-        const width = getWidth(
-          todo.startTime,
-          objectTime,
-          timelineStartTime,
-          timelineEndTime,
-        );
-
-        return (
-          <div key={index}>
-            <Tippy content={`${todo.text}`}>
-              <div
-                className={cn('item', { done: todo.finishTime })}
-                style={{ left: `${left}%`, width: `${width}%` }}
-              >
-                <div className="item-text">{todo.text}</div>
-                {progress}
-              </div>
-            </Tippy>
-          </div>
-        );
-      });
-
-      setItems(result);
+      setTasks(taskElements());
     }, 1000);
 
     return (): void => clearInterval(loop);
@@ -143,7 +129,7 @@ const Timeline = (props: Props): JSX.Element => {
   return (
     <div className="Timeline">
       <div className="graph">
-        {items}
+        {tasks}
         <div className="line"></div>
         <div className="timebox">{timeRuler.map((value) => value)}</div>
       </div>
